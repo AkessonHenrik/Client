715c3fe467d4d27c2830d293478aeafc142d4dd0
AkessonHenrik <henrik.akesson@heig-vd.ch>
2017-05-28 22:44:28 +0200

    Add bootstrap, modify sidebar, add login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  
-  constructor(private router: Router) { }
+  email: string;
+  password: string;
+
+  constructor(private router: Router, private http: Http) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    let credentials = { "email": this.email, "password": this.password }
+    console.log(credentials);
   }
 
 }
